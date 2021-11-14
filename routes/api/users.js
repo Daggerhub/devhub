3e11e52ea194bd77dd3c07d2697c33c22c5ce75d
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2021-11-14 19:13:48 +0530

    Added auth middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
+const config = require('config');
 
 // @route    POST api/users
 // @desc     Register user
@@ -57,8 +58,21 @@ router.post(
 
       await user.save();
 
-      // Return jsonwebtoken
-      res.send('User registered');
+ const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
